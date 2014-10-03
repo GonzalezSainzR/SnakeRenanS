5338de1f3256f9747bb394d9ab1bf46a1c74e2e7
GonzalezSainzR <Raygs2000@gmail.com>
2014-10-03 14:56:11 -0700

    10/3/14
    SNAKE!

---
+++ public_html/js/snake.js
@@ -0,0 +1,21 @@
+var snake;
+
+var context;
+var screenWidth;
+var screenHeight;
+
+function gameInitialize () {
+    var canvas = document.getElementById ("game-screen");
+    context = canvas.getContext ("2d");
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
+            }
+function gameLoop () {
+    
+}
+function gameDraw () {
+    context.fillstyle = "rgb(224, 176, 16)";
+}
